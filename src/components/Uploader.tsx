98399801f2760f70b5920801888d6790cef3be1a
Gnyaund <j.dsh.3naptap@gmail.com>
2022-07-13 12:58:31 +0900

    Add: Write scenario file path to json

--- src/components/Uploader.tsx
+++ src/components/Uploader.tsx
@@ -1,24 +1,42 @@
 import React, { useState } from "react";
 import { Button } from "@mui/material";
+import qualplot from "./../json/qualplot.json";
+import * as fs from "fs";
 
+interface ConfigFile {
+  save?: string;
+  scenario_file_path?: string;
+}
 
 export default function Uploader() {
-    const [path, setPath] = useState("")
-    const handleChange = async () => {
-        //setPath(() => e.target.value)
-        const filePath = await window.electronAPI.openFile()
-        setPath(() => filePath)
-    }//仕様でfakepathになる, pathはjsonに書き込み
+  const [path, setPath] = useState("");
+  const handleChange = async () => {
+    //setPath(() => e.target.value)
+    const filePath: string = await window.electronAPI.openFile();
+    qualplot.scenario_file_path = filePath;
+    const settings: string | ArrayBufferView = JSON.stringify(
+      qualplot,
+      undefined,
+      4
+    );
+    fs.writeFileSync("./../json/qualplot.json", settings);
+    setPath(() => filePath);
+  }; //仕様でfakepathになる, pathはjsonに書き込み
 
-    return(
-        <div>
-            <Button variant="outlined" component="label" color="secondary" id="scenario-file" onClick={handleChange}>
-            Scenario File
-            </Button>
-            <p>{path}</p>
-        </div>
-    )
-} 
+  return (
+    <div>
+      <Button
+        variant="outlined"
+        component="label"
+        color="secondary"
+        id="scenario-file"
+        onClick={handleChange}
+      >
+        Scenario File
+      </Button>
+    </div>
+  );
+}
 //            <button onClick={() => alert(path)}>値の確認</button>
 /*
 e: React.ChangeEvent<HTMLInputElement>)
