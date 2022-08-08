d185a47328416f67f859e5a09669d0b3c0ccf68c
BuildTools <unconfigured@null.spigotmc.org>
2022-08-08 15:07:59 +0900

    Add: can choose 1 - 20

--- src/components/LabelNameResolve.tsx
+++ src/components/LabelNameResolve.tsx
@@ -8,14 +8,10 @@ export default function LabelNameResolve(props: Props) {
   const [value, setValue] = React.useState("");
   const handleChange = (event: SelectChangeEvent) => {
     setValue(event.target.value as string);
-    const value : number = Number(event.target.value)
-    if (props.valuetype === "Start Seed")
-      window.electronAPI.stSeed(value)
-    if (props.valuetype === "End Seed")
-      window.electronAPI.endSeed(value)
-    if (props.valuetype === "Max Node")
-      window.electronAPI.maxNode(value)
-
+    const value: number = Number(event.target.value);
+    if (props.valuetype === "Start Seed") window.electronAPI.stSeed(value);
+    if (props.valuetype === "End Seed") window.electronAPI.endSeed(value);
+    if (props.valuetype === "Max Node") window.electronAPI.maxNode(value);
   };
 
   return (
@@ -37,6 +33,16 @@ export default function LabelNameResolve(props: Props) {
         <MenuItem value={8}>8</MenuItem>
         <MenuItem value={9}>9</MenuItem>
         <MenuItem value={10}>10</MenuItem>
+        <MenuItem value={10}>11</MenuItem>
+        <MenuItem value={10}>12</MenuItem>
+        <MenuItem value={10}>13</MenuItem>
+        <MenuItem value={10}>14</MenuItem>
+        <MenuItem value={10}>15</MenuItem>
+        <MenuItem value={10}>16</MenuItem>
+        <MenuItem value={10}>17</MenuItem>
+        <MenuItem value={10}>18</MenuItem>
+        <MenuItem value={10}>19</MenuItem>
+        <MenuItem value={10}>20</MenuItem>
       </Select>
     </div>
   );
